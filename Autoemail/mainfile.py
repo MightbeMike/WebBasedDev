cac005713cb31cb8593459f42b0052acd9ef63b1
MightbeMike <156243773+MightbeMike@users.noreply.github.com>
2024-02-05 02:05:26 -0500

    Update mainfile.py

--- Autoemail/mainfile.py
+++ Autoemail/mainfile.py
@@ -1,27 +1,38 @@
+#Auto Gmail sender that I used for work.
+#easily constructed to be more veritile and easy to edit
+#used on multiple win OS and Linux
+
+#automation can be accomplished with Win task schedule 
+# or by importing Schedule/Time 
+# into the script and installing "pip install schedule" in Linux
+
 import smtplib
 from email.mime.text import MIMEText
 
-def send_email(subject, body, sender_email, password, recipient_email):
-    # Create a MIMEText object to represent the email body
+def send_email(subject, body, sender_email, password, recipient_file):
+    
+    with open(recipient_file, 'r') as file:
+        recipients = [line.strip() for line in file]
+
     message = MIMEText(body)
     message['Subject'] = subject
     message['From'] = sender_email
-    message['To'] = recipient_email
+    message['To'] = ', '.join(recipients)
 
-    # Establish a connection to Gmail's SMTP server over SSL
+# the fallwing would be edited depending on user acc to use Token
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-        # Login to the SMTP server using the sender's email address and password
         server.login(sender_email, password)
         
-        # Send the email
-        server.sendmail(sender_email, recipient_email, message.as_string())
+        server.sendmail(sender_email, recipients, message.as_string())
 
-# Email details
-subject = "Test Email"
+# the senders Email details
+# info will be for all sent recipents 
+#this can also be edited by having CC recipients for privit messages 
+subject = "What ever your subject is"
 body = "This is a test email sent via Gmail SMTP using Python."
-sender_email = "m.graziano.qfcp@gmail.com"
-password = "Graz1300566"
-recipient_email = "m.graziano@protonmail.com"
+sender_email = "Your Gmail"
+password = "Your Gmail login"
+recipient_file = "recipients.txt"  # Path to the file containing recipients' email addresses
 
-# Send the email
-send_email(subject, body, sender_email, password, recipient_email)
+# Send It!
+send_email(subject, body, sender_email, password, recipient_file)
